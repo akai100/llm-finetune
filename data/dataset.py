89739614357f63e381ccd08ec9bee793d2b2ce4a
Xianglong Zhao <1107971906@qq.com>
2025-12-29 16:00:20 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,1 +1,21 @@
+from torch.utils.data import Dataset
 
+class InstructionDataset(Dataset):
+  def __init__(self, data, tokenizer, max_len):
+    self.data = data
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+
+def _getitem__(self, idx):
+  item = self.data[idx]
+  tokens = self.tokenizer(
+    item["text"],
+    truncation=True,
+    max_length=self.max_len
+  )
+  tokens["labels"] = tokens["input_ids"].copy()
+  return tokens
+
+def __len__(self):
+
+  return len(self.data)
