6cfb9202411433afd38d7c94a79d0af666f00c85
Xianglong Zhao <1107971906@qq.com>
2025-12-29 16:59:44 +0800

    Update load_model.py

--- src/models/load_model.py
+++ src/models/load_model.py
@@ -1,20 +1,22 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import LoraConfig, get_peft_model
 
 def load_model(cfg):
-  tokenizer = AutoTokenizer.from_pretrained(
-    cfg.model_name,
-    use_fast=True,
-    trust_remote_code=True
-  )
-
-  model = AutoModelForCausalLM.from_pretrained(
+    tokenizer = AutoTokenizer.from_pretrained(
         cfg.model_name,
-        torch_dtype=cfg.dtype,
-        device_map=None   # accelerate 接管
-  )
+        use_fast=True,
+        trust_remote_code=True
+    )
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
 
-  if cfg.use_lora:
+    model = AutoModelForCausalLM.from_pretrained(
+        cfg.model_name,
+        torch_dtype=torch.bfloat16 if cfg.bf16 else torch.float16
+    )
+
+    if cfg.use_lora:
         lora_cfg = LoraConfig(
             r=cfg.lora_r,
             lora_alpha=cfg.lora_alpha,
@@ -23,4 +25,6 @@ def load_model(cfg):
             task_type="CAUSAL_LM"
         )
         model = get_peft_model(model, lora_cfg)
-  return model, tokenizer
+        model.print_trainable_parameters()
+
+    return model, tokenizer
