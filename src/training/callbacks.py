de9b2b481d33abbc6d036694b47e04d5a68d41f8
Xianglong Zhao <1107971906@qq.com>
2025-12-29 17:08:14 +0800

    Update callbacks.py

--- src/training/callbacks.py
+++ src/training/callbacks.py
@@ -7,3 +7,10 @@ class NaNLossCallback(TrainerCallback):
         if loss is not None and check_loss(loss, state.global_step):
             control.should_training_stop = True
             return control
+
+class DataMonitorCallback(TrainerCallback):
+    def on_step_end(self, args, state, control, **kwargs):
+        inputs = kwargs.get("inputs")
+        if inputs and "input_ids" in inputs:
+            monitor.update(inputs["input_ids"])
+            monitor.check(state.global_step)
