06a7b42668100a64665de81e60457fd38bf26196
Xianglong Zhao <1107971906@qq.com>
2025-12-29 16:14:13 +0800

    Update preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,1 +1,15 @@
+from data.data_auditor import DataAuditor
+
+def preprocess_dataset(path):
+    auditor = DataAuditor()
+    raw = load_jsonl(path)
+    processed = []
+
+    for s in raw:
+        s["text"] = build_prompt(s)
+        if auditor.audit_sample(s):
+            processed.append(s)
+
+    auditor.report()
+    return processed
 
