884a8199bd3498bacadbf5b87a027083e7e92c2e
Xianglong Zhao <1107971906@qq.com>
2025-12-29 17:03:49 +0800

    Update callbacks.py

--- src/training/callbacks.py
+++ src/training/callbacks.py
@@ -1,1 +1,9 @@
+from transformers import TrainerCallback
+from src.training.nan_detector import check_loss
 
+class NaNLossCallback(TrainerCallback):
+    def on_step_end(self, args, state, control, **kwargs):
+        loss = kwargs.get("loss")
+        if loss is not None and check_loss(loss, state.global_step):
+            control.should_training_stop = True
+            return control
